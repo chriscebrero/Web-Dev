57763f17bb789e79e6f5be613950df4d187a101c
chriscebrero <chris.cebrero@gmail.com>
2018-06-01 16:36:02 -0700

    forloop 3rd commit

---
+++ For-Loop Problem Set/forloopps.js
@@ -0,0 +1,11 @@
+console.log("print all numbers between -10 and 19");
+for(var i= -10; i< 20; i++){
+	console.log(i);
+}
+
+console.log("print all even numbers between 10 and 40");
+for(var i= 10; i <= 40; i++){
+	if (i % 2 === 0) {
+		console.log(i)
+	}
+}
