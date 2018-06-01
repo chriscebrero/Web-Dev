a1e0997a1e5add0408dd79c874fdadbdcb466791
chriscebrero <chris.cebrero@gmail.com>
2018-06-01 16:31:03 -0700

    forloop 1st commit

---
+++ annoyomatic.js
@@ -0,0 +1,13 @@
+var answer = prompt("Are we there yet?");
+
+while(answer !== "yes") {
+	var answer = prompt("How about now?");
+}
+
+alert("We did it!!! We made it!!");
+
+// if(answer === "Yes" || answer === "yes" || answer === "yeah") {
+// 	alert("We did it!!!!! We made it!!");
+// } else {
+// 	var answer = prompt("Are we there now???");
+// }
