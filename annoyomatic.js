04aa4873bc6fc27811d81936fa59309f14b1922b
chriscebrero <chris.cebrero@gmail.com>
2018-06-01 16:11:27 -0700

    3rd commit

--- annoyomatic.js
+++ annoyomatic.js
@@ -1,1 +1,7 @@
 var answer = prompt("Are we there yet?");
+
+if(answer === "Yes" || answer === "yes" || answer === "yeah") {
+	alert("We did it!!!!! We made it!!");
+} else {
+	var answer = prompt("Are we there now???");
+}
