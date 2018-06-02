f65d9ca72e3ab88ba4d79b637a98b08716f24b67
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 12:29:37 -0700

    Clean code

--- Todo List/list.js
+++ Todo List/list.js
@@ -4,23 +4,35 @@ var input = prompt("What would you like to do?")
 
 while(input !== "quit"){
 	if(input === "list") {
-		console.log("**********");
-		todos.forEach(function(todo, i){
-			console.log(i + ": " + todo);
-		});
-		console.log("**********");
+		listTodos();
 } else if(input === "new") {
 	// ask for new todos
-	var newTodo = prompt("Enter a new todo");
-	todos.push(newTodo);
-	console.log("Added Item");
+	newTodos();
 	} else if (input === "delete") {
-		//ask for index of todo to be deleted
-		var index = prompt("Enter index of todo to delete");
-		//delete that todo
-		todos.splice(index, 1);
-		console.log("Item deleted");
+		deleteTodos();
 	}
 	input = prompt("What would you like to do?")
 }
 console.log("Ok, you quit the application")
+
+function listTodos() {
+	console.log("**********");
+		todos.forEach(function(todo, i){
+			console.log(i + ": " + todo);
+		});
+		console.log("**********");
+}
+
+function newTodos() {
+	var newTodo = prompt("Enter a new todo");
+	todos.push(newTodo);
+	console.log("Added Item");
+}
+
+function deleteTodos() {
+	//ask for index of todo to be deleted
+		var index = prompt("Enter index of todo to delete");
+		//delete that todo
+		todos.splice(index, 1);
+		console.log("Item deleted");
+}
