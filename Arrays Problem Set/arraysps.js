526713a493a3c333ab949b8cb21b2e33522d3839
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 12:41:10 -0700

    added printReverse and isUniform

---
+++ Arrays Problem Set/arraysps.js
@@ -0,0 +1,17 @@
+function printReverse(arr) {
+	for (var i = arr.length - 1; i >= 0; i--)
+		console.log(arr[i]);
+}
+
+//*** isUniform() ***
+//
+
+function isUniform(arr) {
+	var first = arr[0];
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] !== first) {
+			return false;
+		}
+	}
+	return true;
+}
