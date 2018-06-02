0527d83b2668857ff3d8934d0d000700a1c5d74d
chriscebrero <chris.cebrero@gmail.com>
2018-06-01 17:45:00 -0700

    idk

--- Function Problem Set/functions.js
+++ Function Problem Set/functions.js
@@ -1,3 +1,24 @@
 function isEven(num) {
-	return (num % 2 == 0);
+	if (num % 2 === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
+
+
+// Factorial Question
+// 
+
+function factorial(num) {
+	// Define a result variable
+	var result = 1;
+	// 
+	// Return the result Variable
+	for (var i = 1; i <= num; i++){
+		result = result * i;
+	}
+	return result
+}
+
