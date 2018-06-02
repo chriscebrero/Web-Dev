7db54be17e64e7de84938abf7c5b0c119eb416b6
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 10:54:51 -0700

    4th todo

--- Todo List/list.js
+++ Todo List/list.js
@@ -2,10 +2,14 @@ var todos = [];
 
 var input = prompt("What would you like to do?")
 
-if(input === "list") {
+while(input !== "quit"){
+	if(input === "list") {
 	console.log(todos);
 } else if(input === "new") {
 	// ask for new todos
 	var newTodo = prompt("Enter a new todo");
 	todos.push(newTodo);
+	}
+	input = prompt("What would you like to do?")
 }
+console.log("Ok, you quit the application")
