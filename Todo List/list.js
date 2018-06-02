902aab25a94aac907e74a72c1aebd2b66fc3692d
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 12:23:43 -0700

    delete functionality

--- Todo List/list.js
+++ Todo List/list.js
@@ -13,6 +13,12 @@ while(input !== "quit"){
 	// ask for new todos
 	var newTodo = prompt("Enter a new todo");
 	todos.push(newTodo);
+	} else if (input === "delete") {
+		//ask for index of todo to be deleted
+		var index = prompt("Enter index of todo to delete");
+		//delete that todo
+		todos.splice(index, 1);
+		console.log("Item deleted");
 	}
 	input = prompt("What would you like to do?")
 }
