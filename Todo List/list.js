2a7e8e2077ba391762cd837fbf027558bf0ac779
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 12:17:31 -0700

    added asterisks

--- Todo List/list.js
+++ Todo List/list.js
@@ -4,7 +4,11 @@ var input = prompt("What would you like to do?")
 
 while(input !== "quit"){
 	if(input === "list") {
-	console.log(todos);
+		console.log("**********");
+		todos.forEach(function(todo, i){
+			console.log(i + ": " + todo);
+		});
+		console.log("**********");
 } else if(input === "new") {
 	// ask for new todos
 	var newTodo = prompt("Enter a new todo");
