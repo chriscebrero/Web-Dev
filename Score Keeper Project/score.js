fca02d5a9d9b2f5ea5a5fd6641b2d7647b89c7a0
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 20:57:44 -0700

    added gameover functionality

--- Score Keeper Project/score.js
+++ Score Keeper Project/score.js
@@ -5,13 +5,24 @@ var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function() {
+	if(!gameOver) {
 	p1Score++;
+	if(winningScore === p1Score) {
+		gameOver = true;
+	}
 	p1Display.textContent = p1Score;
+	}
 });
 
 p2Button.addEventListener("click", function() {
+	if(!gameOver) {
 	p2Score++;
+	if(winningScore === p2Score) {
+		gameOver = true;
+	}
 	p2Display.textContent = p2Score;
+	}
 });
