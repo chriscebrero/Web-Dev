2e9fdedb4909f59f0326e631e871190b06e004ac
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 20:52:43 -0700

    finished player buttons

--- Score Keeper Project/score.js
+++ Score Keeper Project/score.js
@@ -1,14 +1,17 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var h1 = document.querySelector("h1");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
 
 p1Button.addEventListener("click", function() {
 	p1Score++;
-	console.log(p1Score);
-	h1.textContent = p1Score;
+	p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function() {
-	alert("clicked!");
+	p2Score++;
+	p2Display.textContent = p2Score;
 });
