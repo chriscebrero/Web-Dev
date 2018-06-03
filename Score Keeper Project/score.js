dd6a7b6974544dc96bceaef5cab296eeced2673e
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 20:43:32 -0700

    hard

--- Score Keeper Project/score.js
+++ Score Keeper Project/score.js
@@ -1,1 +1,14 @@
-score.js
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+var h1 = document.querySelector("h1");
+var p1Score = 0;
+
+p1Button.addEventListener("click", function() {
+	p1Score++;
+	console.log(p1Score);
+	h1.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function() {
+	alert("clicked!");
+});
