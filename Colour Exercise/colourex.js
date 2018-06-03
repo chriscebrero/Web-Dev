77bf49a6d9d799e37f8c07cd8c4a292fc33c4333
chriscebrero <chris.cebrero@gmail.com>
2018-06-02 19:15:02 -0700

    refactored

--- Colour Exercise/colourex.js
+++ Colour Exercise/colourex.js
@@ -5,10 +5,9 @@ var isMint = false;
 button.addEventListener("click", function () {
 	if (isMint) {
 		document.body.style.background = "white";
-		isMint = false;
 	} else {
 		document.body.style.background = "#98ff98";
-		isMint = true;
 	}
+	isMint = !isMint;
 });
 
